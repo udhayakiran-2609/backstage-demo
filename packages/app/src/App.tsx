c2e51af4018741bc65a4ccf520b54744338b82a0
udhayakaran <udhayakaran@elitecorpusa.com>
2026-06-12 18:44:00 +0530

    fix: add notifications plugin to frontend app

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,13 +1,13 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import { navModule } from './modules/nav';
-
-import { oktaAuthApiRef,googleAuthApiRef  } from '@backstage/core-plugin-api';
+import { oktaAuthApiRef, googleAuthApiRef } from '@backstage/core-plugin-api';
 import { SignInPageBlueprint } from '@backstage/plugin-app-react';
 import { SignInPage } from '@backstage/core-components';
 import { createFrontendModule } from '@backstage/frontend-plugin-api';
 import techRadarPlugin from '@backstage-community/plugin-tech-radar/alpha';
 import { techDocsReportIssueAddonModule } from '@backstage/plugin-techdocs-module-addons-contrib/alpha';
+import notificationsPlugin from '@backstage/plugin-notifications/alpha';
 
 const signInPage = SignInPageBlueprint.make({
   params: {
@@ -15,30 +15,32 @@ const signInPage = SignInPageBlueprint.make({
       (
         <SignInPage
           {...props}
-           providers={[
-          {
-            id: 'okta-auth-provider',
-            title: 'Okta',
-            message: 'Sign in with your Okta account',
-            apiRef: oktaAuthApiRef,
-          },
-          {
-            id: 'google-auth-provider',
-            title: 'Google Workspace',
-            message: 'Sign in with your Google account',
-            apiRef: googleAuthApiRef,
-          },
-        ]}
+          providers={[
+            {
+              id: 'okta-auth-provider',
+              title: 'Okta',
+              message: 'Sign in with your Okta account',
+              apiRef: oktaAuthApiRef,
+            },
+            {
+              id: 'google-auth-provider',
+              title: 'Google Workspace',
+              message: 'Sign in with your Google account',
+              apiRef: googleAuthApiRef,
+            },
+          ]}
         />
       ),
   },
 });
+
 export default createApp({
   features: [
     catalogPlugin,
     navModule,
     techRadarPlugin,
     techDocsReportIssueAddonModule,
+    notificationsPlugin,
     createFrontendModule({
       pluginId: 'app',
       extensions: [signInPage],
