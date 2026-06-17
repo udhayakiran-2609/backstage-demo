d0913fb450478554f0104c0231acf3338cbdf356
udhayakaran <udhayakaran@elitecorpusa.com>
2026-06-17 14:47:11 +0530

    backstage deplopyment

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -26,7 +26,6 @@ backend.add(
 backend.add(import('@backstage/plugin-techdocs-backend'));
 // backend.add(import('@backstage/plugin-catalog-backend-module-github'));
 
-
 // auth plugin
 backend.add(import('@backstage/plugin-auth-backend'));
 // See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
